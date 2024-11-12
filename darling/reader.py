97d4ccc78138ca8f2620d72b2fa3d599f79a8b64
Axel Henningsson <nilsaxelhenningsson@gmail.com>
2024-11-12 08:46:23 +0100

    Added energy scan reader for id03

--- darling/reader.py
+++ darling/reader.py
@@ -8,6 +8,8 @@ all features of darling.
 
 """
 
+import re
+
 import h5py
 import hdf5plugin
 import matplotlib.pyplot as plt
@@ -25,7 +27,7 @@ class Reader(object):
     def __init__(self, abs_path_to_h5_file):
         self.abs_path_to_h5_file = abs_path_to_h5_file
 
-    def read_scan(self, args, scan_id, roi=None):
+    def __call__(self, args, scan_id, roi=None):
         """Method to read a single 2D scan
 
         NOTE: This method is meant to be purpose implemented to fit the specific data aqusition
@@ -35,14 +37,14 @@ class Reader(object):
             args str (:obj:`list`): list of arguments needed by the reader.
             scan_id (:obj:`str`): scan id to load from, these are internal kayes to diffirentiate
                 layers.
-            roi (:obj:`tuple` of :obj:`int`): row_min row_max and column_min and column_max, 
+            roi (:obj:`tuple` of :obj:`int`): row_min row_max and column_min and column_max,
                 defaults to None, in which case all data is loaded. The roi refers to the detector
                 dimensions.
 
         Returns:
-            data (:obj:`numpy array`) of shape=(a,b,m,n) and type np.uint16 and motors 
+            data (:obj:`numpy array`) of shape=(a,b,m,n) and type np.uint16 and motors
             (:obj:`tuple` of :obj:`numpy array`) of shape=(m,) and shape=(n,) and type
-            np.float32. a,b are detector dimensions while m,n are scan dimensions over 
+            np.float32. a,b are detector dimensions while m,n are scan dimensions over
             which teh motor settings vary.
 
         """
@@ -50,7 +52,11 @@ class Reader(object):
 
 
 class MosaScan(Reader):
-    """Load a 2D mosa scan.
+    """Load a 2D mosa scan. This is a id03 specific implementation matching a specific beamline mosa scan macro.
+
+    NOTE: This reader was specifically written for data collection at id03. For general purpose reading of data you
+    must implement your own reader class. The exact reding of data is strongly dependent on data aqusition scheme and
+    data structure implementation.
 
     Args:
         abs_path_to_h5_file str (:obj:`str`): absolute path to the h5 file with the diffraction images.
@@ -66,23 +72,24 @@ class MosaScan(Reader):
         motor_precision,
     ):
         self.abs_path_to_h5_file = abs_path_to_h5_file
-        self.motornames = motor_names
+        self.motor_names = motor_names
         self.motor_precision = motor_precision
 
         assert len(self.motor_precision) == len(
-            self.motornames
-        ), "The motor_precision lengths need to match the motornames length"
+            self.motor_names
+        ), "The motor_precision lengths need to match the motor_names length"
 
-    def read_scan(self, data_name, scan_id, roi=None):
+    def __call__(self, data_name, scan_id, roi=None):
         """Load a scan
 
         this loads the mosa data array with shape N,N,m,n where N is the detector dimension and
-        m,n are the motor dimensions as ordered in the self.motor_names.
+        m,n are the motor dimensions as ordered in the self.motor_names. You may view the implemented darling readers as example templates for implementing
+        your own reader.
 
         Args:
             data_name (:obj:`str`): path to the data wihtout the prepended scan id
             scan_id (:obj:`str`):scan id to load from, e.g 1.1, 2.1 etc...
-            roi (:obj:`tuple` of :obj:`int`): row_min row_max and column_min and column_max, 
+            roi (:obj:`tuple` of :obj:`int`): row_min row_max and column_min and column_max,
                 defaults to None, in which case all data is loaded
 
         Returns:
@@ -90,8 +97,9 @@ class MosaScan(Reader):
 
         """
         with h5py.File(self.abs_path_to_h5_file, "r") as h5f:
+
             # Read in motors
-            raw_motors = [h5f[scan_id][mn] for mn in self.motornames]
+            raw_motors = [h5f[scan_id][mn] for mn in self.motor_names]
             motors = [
                 np.unique(np.round(m, p)).astype(np.float32)
                 for p, m in zip(self.motor_precision, raw_motors)
@@ -110,11 +118,104 @@ class MosaScan(Reader):
             )
             data = data.swapaxes(0, 2)
             data = data.swapaxes(1, -1)
-        
+
         return data, motors
 
-# TODO: implement the strain scan reader.
-# TODO: test these classe with h5 files from id03.
+
+class EnergyScan(Reader):
+    """Load a 2D energy scan. This is a id03 specific implementation matching a specific beamline energy scan macro.
+
+    NOTE: This reader was specifically written for data collection at id03. For general purpose reading of data you
+    must implement your own reader class. The exact reding of data is strongly dependent on data aqusition scheme and
+    data structure implementation. You may view the implemented darling readers as example templates for implementing
+    your own reader.
+
+    Args:
+        abs_path_to_h5_file str (:obj:`str`): absolute path to one of the h5 file with the diffraction images. FOr an energy
+            scan there is one file per layer in z. This may be any one of these paths. Provided the file name ends with
+            layer_0.h5 the file path will be rebuilt upon read call.
+        motor_names data (:obj:`list` of :obj:`str`): h5 paths to the data these need to be ordered exactly as [energy, chi]
+            In an energy scan the h5 grouping is in energy and only the subsequent motor srquence need to be provided. I.e
+            for instance '/instrument/chi/value' could be the path to the chi motor.
+        motor_precision data (:obj:`list` of :obj:`int`): number of trusted deciamls in each motor dimension. (matching motor_names)
+    """
+
+    def __init__(
+        self,
+        abs_path_to_h5_file,
+        motor_names,
+        motor_precision,
+    ):
+        self.abs_path_to_h5_file = abs_path_to_h5_file
+        self.motor_names = motor_names
+        self.motor_precision = motor_precision
+
+        assert len(self.motor_precision) == len(
+            self.motor_names
+        ), "The motor_precision lengths need to match the motor_names length"
+
+    def _get_layer_path(self, scan_id):
+        layer_number = scan_id[0]
+        layer_tag = r"layer_" + layer_number + ".h5"
+        return re.sub(r"layer_\d+\.h5", layer_tag, self.abs_path_to_h5_file)
+
+    def __call__(self, data_name, scan_id, roi=None):
+        """Load a scan
+
+        this loads the mosa data array with shape N,N,m,n where N is the detector dimension and
+        m,n are the motor dimensions as ordered in the self.motor_names.
+
+        Args:
+            data_name (:obj:`str`): path to the data wihtout the prepended energy id. i,e
+                '/instrument/pco_ff/data' or the like.
+            scan_id (:obj:`str`):scan id to load from, e.g 1.1, 2.1 etc...
+            roi (:obj:`tuple` of :obj:`int`): row_min row_max and column_min and column_max,
+                defaults to None, in which case all data is loaded
+
+        Returns:
+            data, motors : data of shape (a,b,m,n) and motors tuple of len=m and len=n
+
+        """
+        abs_path_to_h5_file = self._get_layer_path(scan_id)
+
+        with h5py.File(self.abs_path_to_h5_file, "r") as h5f:
+            key0 = list(h5f.keys())[0]
+
+            chi = h5f[key0 + self.motor_names[1]][:].astype(np.float32)
+
+            _, det_rows, det_cols = h5f[key0 + data_name].shape
+            n_energy = len(h5f.keys())
+            n_chis = len(chi)
+
+            if roi is None:
+                data = np.zeros((det_rows, det_cols, n_energy, n_chis), dtype=np.uint16)
+            else:
+                r1, r2, c1, c2 = roi
+                data = np.zeros((r2 - r1, c2 - c1, n_energy, n_chis), dtype=np.uint16)
+
+            energy = np.zeros((n_energy,), dtype=np.float32)
+
+            all_chis = []
+
+            for i, key in enumerate(h5f.keys()):  # iterates over energies.
+                chi_stack = h5f[key + data_name][:, :, :]
+                chi_stack = np.swapaxes(chi_stack, 0, 1)
+                chi_stack = np.swapaxes(chi_stack, 1, 2)
+                if roi is None:
+                    data[:, :, i, :] = chi_stack
+                else:
+                    data[:, :, i, :] = chi_stack[r1:r2, c1:c2, :]
+                energy[i] = h5f[key + self.motor_names[0]][()]
+                all_chis.extend(list(h5f[key0 + self.motor_names[1]][:]))
+
+        chi = np.unique(np.round(all_chis, self.motor_precision[0])).astype(np.float32)
+        energy = np.round(energy, self.motor_precision[0]).astype(np.float32)
+        motors = [energy, chi]
+
+        assert len(chi) == data.shape[3], "Potential motor drift in chi"
+
+        return data, motors
+
 
 if __name__ == "__main__":
     pass
--- tests/test_reader.py
+++ tests/test_reader.py
@@ -24,7 +24,7 @@ class TestMosaScan(unittest.TestCase):
         )
 
     def test_read(self):
-        # assert that the reader will read data and coordinates of the correct type and 
+        # assert that the reader will read data and coordinates of the correct type and
         # expected shapes.
         reader = darling.reader.MosaScan(
             self.path_to_data,
@@ -32,22 +32,20 @@ class TestMosaScan(unittest.TestCase):
             motor_precision=[2, 2],
         )
 
-        print(reader)
-
-        data, motors = reader.read_scan(
+        data, motors = reader(
             data_name="instrument/pco_ff/image",
             scan_id="1.1",
         )
 
-        self.assertTrue(data.dtype==np.uint16)
-        self.assertTrue(len(data.shape)==4)
-        self.assertTrue(data.shape[2]==len(motors[0]))
-        self.assertTrue(data.shape[3]==len(motors[1]))
-        self.assertTrue(motors[0].dtype==np.float32)
-        self.assertTrue(motors[1].dtype==np.float32)
-        self.assertTrue(len(motors[0].shape)==1)
-        self.assertTrue(len(motors[1].shape)==1)
-    
+        self.assertTrue(data.dtype == np.uint16)
+        self.assertTrue(len(data.shape) == 4)
+        self.assertTrue(data.shape[2] == len(motors[0]))
+        self.assertTrue(data.shape[3] == len(motors[1]))
+        self.assertTrue(motors[0].dtype == np.float32)
+        self.assertTrue(motors[1].dtype == np.float32)
+        self.assertTrue(len(motors[0].shape) == 1)
+        self.assertTrue(len(motors[1].shape) == 1)
+
     def test_roi_read(self):
         # assert that the reader will read a roi
         reader = darling.reader.MosaScan(
@@ -56,23 +54,21 @@ class TestMosaScan(unittest.TestCase):
             motor_precision=[2, 2],
         )
 
-        data, motors = reader.read_scan(
-            data_name="instrument/pco_ff/image",
-            scan_id="1.1",
-            roi=(10, 20, 0, 7)
+        data, motors = reader(
+            data_name="instrument/pco_ff/image", scan_id="1.1", roi=(10, 20, 0, 7)
         )
 
-        self.assertTrue(data.dtype==np.uint16)
-        self.assertTrue(len(data.shape)==4)
-        self.assertTrue(data.shape[0]==10)
-        self.assertTrue(data.shape[1]==7)
-        self.assertTrue(data.shape[2]==len(motors[0]))
-        self.assertTrue(data.shape[3]==len(motors[1]))
-        self.assertTrue(motors[0].dtype==np.float32)
-        self.assertTrue(motors[1].dtype==np.float32)
-        self.assertTrue(len(motors[0].shape)==1)
-        self.assertTrue(len(motors[1].shape)==1)
-    
+        self.assertTrue(data.dtype == np.uint16)
+        self.assertTrue(len(data.shape) == 4)
+        self.assertTrue(data.shape[0] == 10)
+        self.assertTrue(data.shape[1] == 7)
+        self.assertTrue(data.shape[2] == len(motors[0]))
+        self.assertTrue(data.shape[3] == len(motors[1]))
+        self.assertTrue(motors[0].dtype == np.float32)
+        self.assertTrue(motors[1].dtype == np.float32)
+        self.assertTrue(len(motors[0].shape) == 1)
+        self.assertTrue(len(motors[1].shape) == 1)
+
     def test_scan_id(self):
         # ensure that another scan id can be read
         # assert that the reader will read a roi
@@ -82,19 +78,107 @@ class TestMosaScan(unittest.TestCase):
             motor_precision=[2, 2],
         )
 
-        data, motors = reader.read_scan(
+        data, motors = reader(
             data_name="instrument/pco_ff/image",
             scan_id="2.1",
         )
 
-        self.assertTrue(data.dtype==np.uint16)
-        self.assertTrue(len(data.shape)==4)
-        self.assertTrue(data.shape[2]==len(motors[0]))
-        self.assertTrue(data.shape[3]==len(motors[1]))
-        self.assertTrue(motors[0].dtype==np.float32)
-        self.assertTrue(motors[1].dtype==np.float32)
-        self.assertTrue(len(motors[0].shape)==1)
-        self.assertTrue(len(motors[1].shape)==1)
-    
+        self.assertTrue(data.dtype == np.uint16)
+        self.assertTrue(len(data.shape) == 4)
+        self.assertTrue(data.shape[2] == len(motors[0]))
+        self.assertTrue(data.shape[3] == len(motors[1]))
+        self.assertTrue(motors[0].dtype == np.float32)
+        self.assertTrue(motors[1].dtype == np.float32)
+        self.assertTrue(len(motors[0].shape) == 1)
+        self.assertTrue(len(motors[1].shape) == 1)
+
+
+class TestEnergyScan(unittest.TestCase):
+    # Tests for the darling.MosaScan class.
+
+    def setUp(self):
+        self.debug = False
+        self.path_to_data, _, _ = darling.assets.energy_scan()
+        self.motor_names = ["/instrument/positioners/ccmth", "/instrument/chi/value"]
+        self.motor_precision = [4, 4]
+
+    def test_init(self):
+        # Assert that the reader can be instantiated.
+        reader = darling.reader.EnergyScan(
+            self.path_to_data,
+            self.motor_names,
+            self.motor_precision,
+        )
+
+    def test_read(self):
+        # assert that the reader will read data and coordinates of the correct type and
+        # expected shapes.
+        reader = darling.reader.EnergyScan(
+            self.path_to_data,
+            self.motor_names,
+            self.motor_precision,
+        )
+
+        data, motors = reader(
+            data_name="/instrument/pco_ff/data",
+            scan_id="1.1",
+        )
+
+        self.assertTrue(data.dtype == np.uint16)
+        self.assertTrue(len(data.shape) == 4)
+        self.assertTrue(data.shape[2] == len(motors[0]))
+        self.assertTrue(data.shape[3] == len(motors[1]))
+        self.assertTrue(motors[0].dtype == np.float32)
+        self.assertTrue(motors[1].dtype == np.float32)
+        self.assertTrue(len(motors[0].shape) == 1)
+        self.assertTrue(len(motors[1].shape) == 1)
+
+    def test_roi_read(self):
+        # assert that the reader will read a roi
+        reader = darling.reader.EnergyScan(
+            self.path_to_data,
+            self.motor_names,
+            self.motor_precision,
+        )
+
+        data, motors = reader(
+            data_name="/instrument/pco_ff/data", scan_id="1.1", roi=(10, 20, 0, 7)
+        )
+
+        self.assertTrue(data.dtype == np.uint16)
+        self.assertTrue(len(data.shape) == 4)
+        self.assertTrue(data.shape[0] == 10)
+        self.assertTrue(data.shape[1] == 7)
+        self.assertTrue(data.shape[2] == len(motors[0]))
+        self.assertTrue(data.shape[3] == len(motors[1]))
+        self.assertTrue(motors[0].dtype == np.float32)
+        self.assertTrue(motors[1].dtype == np.float32)
+        self.assertTrue(len(motors[0].shape) == 1)
+        self.assertTrue(len(motors[1].shape) == 1)
+
+    def test_scan_id(self):
+        # ensure that another scan id can be read
+        # assert that the reader will read a roi
+        reader = darling.reader.EnergyScan(
+            self.path_to_data,
+            self.motor_names,
+            self.motor_precision,
+        )
+
+        data, motors = reader(
+            data_name="/instrument/pco_ff/data",
+            scan_id="2.1",
+        )
+
+        self.assertTrue(data.dtype == np.uint16)
+        self.assertTrue(len(data.shape) == 4)
+        self.assertTrue(data.shape[2] == len(motors[0]))
+        self.assertTrue(data.shape[3] == len(motors[1]))
+        self.assertTrue(motors[0].dtype == np.float32)
+        self.assertTrue(motors[1].dtype == np.float32)
+        self.assertTrue(len(motors[0].shape) == 1)
+        self.assertTrue(len(motors[1].shape) == 1)
+
+
 if __name__ == "__main__":
     unittest.main()
