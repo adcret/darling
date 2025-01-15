fix for motor precision bug

darling/_dataset.py
@@ -57,7 +57,7 @@ class _Visualizer(object):
         plt.show()
 
     def covariance(self, mask=None):
-        """
+        """ 
         Plot the covariance matrix of the data set. Using RGBA colormap to plot the covariance matrix with transparency.
 
         Args:
@@ -72,20 +72,20 @@ class _Visualizer(object):
         im_ratio = self.dset.covariance.shape[0] / self.dset.covariance.shape[1]
 
         if mask is not None:
-            mask = mask.astype(bool)
+            mask = mask.astype(bool) 
 
         for i in range(2):
             for j in range(2):
                 data = self.dset.covariance[:, :, i, j]
 
                 if mask is not None:
-                    alpha_channel = np.where(mask, 1, 0)
-                    rgba_data = plt.cm.magma(data)
-                    rgba_data[..., 3] = alpha_channel
+                    alpha_channel = np.where(mask, 1, 0) 
+                    rgba_data = plt.cm.magma(data)  
+                    rgba_data[..., 3] = alpha_channel  
                 else:
-                    rgba_data = plt.cm.magma(data)
+                    rgba_data = plt.cm.magma(data) 
 
-                im = ax[i, j].imshow(rgba_data, interpolation="nearest")
+                im = ax[i, j].imshow(rgba_data, interpolation='nearest')
                 fig.colorbar(im, ax=ax[i, j], fraction=0.046 * im_ratio, pad=0.04)
 
                 ax[i, j].set_title(
@@ -156,7 +156,7 @@ class _Visualizer(object):
         colormap = hsv_to_rgb(hsv_key)
         return colormap
 
-    def mosaicity(self, mask=None):
+    def mosaicity(self, mask = None):
         """
         Plot the mosaicity map. This takes the motor limits in order to avoid issues with zeros in the data.
         Sets the blue channel to 0.75 to make the mosaicity map more readable. The colormap is plotted on the right based on the motor limits.
@@ -164,30 +164,33 @@ class _Visualizer(object):
         Args:
             mask (:obj:`numpy array`): A boolean mask with the same shape as the data set. If provided, the
                 mosaicity map will be plotted with transparency according to the mask values. Defaults to None.
-
+    
         """
         motor1_min, motor1_max = self.dset.motors[0].min(), self.dset.motors[0].max()
         motor2_min, motor2_max = self.dset.motors[1].min(), self.dset.motors[1].max()
 
+    
         mean = self.dset.mean.copy()
-
+    
         mean[:, :, 0] = np.clip(mean[:, :, 0], motor1_min, motor1_max)
         mean[:, :, 1] = np.clip(mean[:, :, 1], motor2_min, motor2_max)
 
+        
         chi_scaled = (mean[:, :, 0] - motor1_min) / (motor1_max - motor1_min)
         phi_scaled = (mean[:, :, 1] - motor2_min) / (motor2_max - motor2_min)
 
         if mask is not None:
-            mask = mask.astype(bool)
+            mask = mask.astype(bool) 
             chi_scaled = np.where(mask, chi_scaled, np.nan)
             phi_scaled = np.where(mask, phi_scaled, np.nan)
 
+
         mosa = np.stack((chi_scaled, phi_scaled, np.ones_like(chi_scaled)), axis=-1)
-        mosa[np.isnan(mosa)] = 0
-        mosa[mosa > 1] = 1
-        mosa[mosa < 0] = 0
+        mosa[np.isnan(mosa)] = 0  
+        mosa[mosa > 1] = 1  
+        mosa[mosa < 0] = 0  
         RGB_scaled = hsv_to_rgb(mosa)
-        RGB_scaled[..., 2] *= 0.75
+        RGB_scaled[..., 2] *= 0.75  
         colormap = self._hsv_colormap()
         colormap[..., 2] *= 0.75
 
@@ -215,17 +218,19 @@ class _Visualizer(object):
         axs[1].set_title(r"Color Map", fontsize=14)
 
         chiTicks = np.linspace(0, colormap.shape[1] - 1, 5)
-        chi_labels = np.linspace(motor1_min, motor1_max, 5)
+        chi_labels = np.linspace(motor1_min, motor1_max, 5)  
         axs[1].set_xticks(chiTicks)
         axs[1].set_xticklabels([f"{chi:.3f}" for chi in chi_labels])
 
         phiTicks = np.linspace(0, colormap.shape[0] - 1, 5)
-        phi_labels = np.linspace(motor2_min, motor2_max, 5)
+        phi_labels = np.linspace(motor2_min, motor2_max, 5)  
         axs[1].set_yticks(phiTicks)
         axs[1].set_yticklabels([f"{phi:.3f}" for phi in phi_labels])
 
         plt.tight_layout()
         plt.show()
+
+
 
 
 class DataSet(object):
@@ -250,7 +255,7 @@ class DataSet(object):
         self.mean, self.covariance = None, None
         self.mean_3d, self.covariance_3d = None, None
 
-    def load_scan(self, args, scan_id, roi=None, scan_size=None):
+    def load_scan(self, args, scan_id, roi=None):
         """Load a scan into RM.
 
         NOTE: Input args should match the darling.reader.Reader used, however it was implemented.
@@ -269,11 +274,11 @@ class DataSet(object):
 
         """
         if isinstance(args, tuple):
-            self.data, self.motors = self.reader(*args, scan_id, roi, scan_size)
+            self.data, self.motors = self.reader(*args, scan_id, roi)
         else:
-            self.data, self.motors = self.reader(args, scan_id, roi, scan_size)
+            self.data, self.motors = self.reader(args, scan_id, roi)
 
-    def substract(self, value):
+    def subtract(self, value):
         """Subtract a fixed integer value form the data. Protects against uint16 sign flips.
 
         Args:
@@ -291,7 +296,7 @@ class DataSet(object):
         the value corresponding to the 99.99% percentile is returned. I.e the far tail of the noise is returned.
 
         """
-        sample_size = 200000
+        sample_size = 40000
         index = np.random.permutation(sample_size)
         sample = self.data.flat[index]
         sample = np.sort(sample)
@@ -493,106 +498,6 @@ class DataSet(object):
             cells,
             point_data=point_data,
         ).write(filename)
-
-    @numba.guvectorize(
-        [
-            (numba.uint16[:, :, :], numba.float32[:], numba.uint16[:, :, :]),
-        ],
-        "(x,y,z),(d)->(x,y,z)",
-        nopython=True,
-        target="parallel",
-    )
-    def _shift(image, shift_vector, output):
-        """
-        3D image shifting.
-
-        Args:
-            image (:obj:`numpy array`): 3D array to shift.
-            shift_vector (:obj:`numpy array`): 3D shift vector.
-            output (:obj:`numpy array`): 3D output array for the shifted image.
-        """
-        shift_x, shift_y, shift_z = map(int, shift_vector)
-        x_size, y_size, z_size = image.shape
-
-        for x in range(x_size):
-            for y in range(y_size):
-                for z in range(z_size):
-                    new_x = x + shift_x
-                    new_y = y + shift_y
-                    new_z = z + shift_z
-
-                    if (
-                        0 <= new_x < x_size
-                        and 0 <= new_y < y_size
-                        and 0 <= new_z < z_size
-                    ):
-                        output[new_x, new_y, new_z] = image[x, y, z]
-                    else:
-                        output[x, y, z] = 0
-
-    def correct_shifts(self, num_subvolumes=5, subvolume_size=None):
-        """
-        Optimized shift correction using FFT-based cross-correlation and Numba for shifting.
-
-        Args:
-            num_subvolumes (:obj:`int`): Number of subvolumes to use for shift estimation.
-            subvolume_size (:obj:`tuple` of :obj:`int`): Size of the subvolume used for shift estimation.
-        """
-        reference_image = self.data[..., 0].astype(np.uint16)
-        corrected_data = np.zeros_like(self.data, dtype=np.uint16)
-        corrected_data[..., 0] = reference_image
-
-        if subvolume_size is None:
-            subvolume_size = tuple([int(0.1 * s) for s in reference_image.shape])
-        print("Subvolume size:", subvolume_size)
-
-        for i in range(1, self.data.shape[-1]):
-            target_image = self.data[..., i].astype(np.uint16)
-            shifts = []
-
-            for _ in range(num_subvolumes):
-                start_idx = [
-                    np.random.randint(0, ref_dim - sub_dim)
-                    for ref_dim, sub_dim in zip(reference_image.shape, subvolume_size)
-                ]
-                end_idx = [
-                    start + size for start, size in zip(start_idx, subvolume_size)
-                ]
-
-                subvol_ref = reference_image[
-                    start_idx[0] : end_idx[0],
-                    start_idx[1] : end_idx[1],
-                    start_idx[2] : end_idx[2],
-                ]
-                subvol_tgt = target_image[
-                    start_idx[0] : end_idx[0],
-                    start_idx[1] : end_idx[1],
-                    start_idx[2] : end_idx[2],
-                ]
-
-                correlation = fftconvolve(
-                    subvol_tgt.astype(np.float32),
-                    subvol_ref[::-1, ::-1, ::-1].astype(np.float32),
-                    mode="same",
-                )
-                max_corr_idx = np.unravel_index(
-                    np.argmax(correlation), correlation.shape
-                )
-
-                center = np.array(correlation.shape) // 2
-                shift_vector = np.array(max_corr_idx) - center
-                shifts.append(shift_vector)
-
-            avg_shift = np.rint(np.mean(shifts, axis=0)).astype(np.float32)
-            print(f"Image {i}: Average shift:", avg_shift)
-
-            corrected_image = np.zeros_like(target_image, dtype=np.uint16)
-            self._shift(target_image, avg_shift, corrected_image)
-            corrected_data[..., i] = corrected_image
-
-        self.data = corrected_data
-        print("Shifts corrected.")
-
 
 if __name__ == "__main__":
     pass

darling/reader.py
@@ -31,7 +31,7 @@ class Reader(object):
     def __init__(self, abs_path_to_h5_file):
         self.abs_path_to_h5_file = abs_path_to_h5_file
 
-    def __call__(self, args, scan_id, roi=None):
+    def __call__(self, args, scan_id, roi=None, scan_size=None):
         """Method to read a single 2D scan
 
         NOTE: This method is meant to be purpose implemented to fit the specific data aqusition
@@ -97,7 +97,8 @@ class MosaScan(Reader):
                 defaults to None, in which case all data is loaded
             scan_size (:obj:`tuple` of :obj:`int`): size of the scan in each motor dimension. This is used to,
                 adjust the motors if the precision is not enough to uniquely identify the motor settings. This is
-                a last resort and should be avoided if possible.
+                a last resort and should be avoided if possible. This should be defined in the same order as the
+                motor_names.
 
         Returns:
             data, motors : data of shape (a,b,m,n) and motors tuple of len=m and len=n
@@ -127,15 +128,11 @@ class MosaScan(Reader):
                 )
             except ValueError as e:
                 if scan_size:
-                    print(
-                        f"Motor precision issue, trying to fixing by setting the scan size to {scan_size}"
-                    )
+                    print(f"Motor precision issue, trying to fixing by setting the scan size to {scan_size}")
                     adjusted_motors = []
                     for i, size in enumerate(scan_size):
                         adjusted_motors.append(
-                            np.linspace(
-                                motors[i][0], motors[i][-1], size, dtype=np.float32
-                            )
+                            np.linspace(motors[i][0], motors[i][-1], size, dtype=np.float32)
                         )
                     motors = adjusted_motors
 
@@ -146,7 +143,7 @@ class MosaScan(Reader):
                 else:
                     print("Motor precision issue, please provide scan size")
                     raise e
-
+                
             data = data.swapaxes(0, 2)
             data = data.swapaxes(1, -1)
 
@@ -260,7 +257,6 @@ class EnergyScan(Reader):
         assert len(chi) == data.shape[3], "Potential motor drift in chi"
 
         return data, motors
-
 
 if __name__ == "__main__":
     pass

tests/test_dataset.py
@@ -4,215 +4,52 @@ import unittest
 import matplotlib.pyplot as plt
 import numpy as np
 
-import darling
+from darling.assets import energy_scan, gaussian_blobs, mosaicity_scan
 
 
-class TestDataSet(unittest.TestCase):
+class TestAssets(unittest.TestCase):
     # Tests for the darling.assets module.
 
     def setUp(self):
         self.debug = False
 
-        # we test for the mosa scan reader
-        path_to_data_1, _, _ = darling.assets.mosaicity_scan()
-        self.reader_1 = darling.reader.MosaScan(
-            path_to_data_1,
-            ["instrument/diffrz/data", "instrument/chi/value"],
-            motor_precision=[2, 2],
-        )
-        self.data_name_1 = "instrument/pco_ff/image"
+    def test_mosaicity_scan(self):
+        path, data, coordinates = mosaicity_scan()
 
-        # as well as the energy scan reader
-        path_to_data_2, _, _ = darling.assets.energy_scan()
-        self.reader_2 = darling.reader.EnergyScan(
-            path_to_data_2,
-            ["/instrument/positioners/ccmth", "/instrument/chi/value"],
-            [4, 4],
-        )
-        self.data_name_2 =  "/instrument/pco_ff/data"
+        self.assertTrue(isinstance(path, str))
+        self.assertTrue(data.dtype == np.uint16)
+        self.assertTrue(len(data.shape) == 4)
+        self.assertTrue(data.shape[2] == len(coordinates[0]))
+        self.assertTrue(data.shape[3] == len(coordinates[1]))
+        self.assertTrue(coordinates[0].dtype == np.float32)
+        self.assertTrue(coordinates[1].dtype == np.float32)
+        self.assertTrue(len(coordinates[0].shape) == 1)
+        self.assertTrue(len(coordinates[1].shape) == 1)
 
-        self.readers = [self.reader_1, self.reader_2]
-        self.data_names = [self.data_name_1, self.data_name_2]
+    def test_energy_scan(self):
+        path, data, coordinates = energy_scan()
 
-    def test_init(self):
-        for reader in self.readers:
-            dset = darling.DataSet(reader)
+        self.assertTrue(isinstance(path, str))
+        self.assertTrue(data.dtype == np.uint16)
+        self.assertTrue(len(data.shape) == 4)
+        self.assertTrue(data.shape[2] == len(coordinates[0]))
+        self.assertTrue(data.shape[3] == len(coordinates[1]))
+        self.assertTrue(coordinates[0].dtype == np.float32)
+        self.assertTrue(coordinates[1].dtype == np.float32)
+        self.assertTrue(len(coordinates[0].shape) == 1)
+        self.assertTrue(len(coordinates[1].shape) == 1)
 
-    def test_load_scan(self):
-        for reader, data_name in zip(self.readers, self.data_names):
-            dset = darling.DataSet(reader)
+    def test_gaussian_blobs(self):
+        m = 5
+        N = 19
+        data, coordinates = gaussian_blobs(N=N, m=m)
 
-            # test that a scan can be loaded.
-            args = data_name
-            dset.load_scan(args, scan_id="1.1", roi=None)
-            self.check_data(dset)
+        self.assertTrue(len(data.shape) == 4)
+        self.assertTrue(data.shape[2] == m)
+        self.assertTrue(data.shape[3] == m)
+        self.assertTrue(len(coordinates[0].shape) == 1)
+        self.assertTrue(len(coordinates[1].shape) == 1)
 
-            # test the tuple args option
-            args = (data_name,)
-            dset.load_scan(args, scan_id="1.1", roi=None)
-            self.check_data(dset)
-            data_layer_1 = dset.data.copy()
-
-            # test to load a diffrent layer
-            args = (data_name,)
-            dset.load_scan(args, scan_id="2.1", roi=None)
-            self.check_data(dset)
-
-            # ensure the data shape is consistent between layers
-            self.assertEqual(data_layer_1.shape, dset.data.shape)
-
-            # ensure the data is actually different between layers.
-            residual = data_layer_1 - dset.data
-            self.assertNotEqual( np.max(np.abs(residual)), 0)
-
-            # test that a roi can be loaded and that the resulting shape is ok.
-            args = (data_name,)
-            dset.load_scan(args, scan_id="2.1", roi=(0, 9, 3, 19))
-            self.check_data(dset)
-            self.assertTrue(dset.data.shape[0] == 9)
-            self.assertTrue(dset.data.shape[1] == 16)
-
-    def test_subtract(self):
-        for reader, data_name in zip(self.readers, self.data_names):
-            dset = darling.DataSet(reader)
-            dset.load_scan(data_name, scan_id="1.1", roi=None)
-            mm = np.max(dset.data)
-            dset.subtract(value=200)
-            self.assertEqual(np.max(dset.data), mm - 200)
-
-    def test_moments(self):
-        for reader, data_name in zip(self.readers, self.data_names):
-            dset = darling.DataSet(reader)
-            dset.load_scan(data_name, scan_id="1.1", roi=None)
-            mean, covariance = dset.moments()
-            self.assertEqual(mean.shape[0], dset.data.shape[0])
-            self.assertEqual(mean.shape[1], dset.data.shape[1])
-            self.assertEqual(mean.shape[2], 2)
-            self.assertEqual(covariance.shape[0], dset.data.shape[0])
-            self.assertEqual(covariance.shape[1], dset.data.shape[1])
-            self.assertEqual(covariance.shape[2], 2)
-            self.assertEqual(covariance.shape[3], 2)
-
-            if self.debug:
-                dset.plot.mean()
-
-    def test_estimate_mask(self):
-        for reader, data_name in zip(self.readers, self.data_names):
-            dset = darling.DataSet(reader)
-            dset.load_scan(data_name, scan_id="1.1", roi=None)
-            mask = dset.estimate_mask()
-            self.assertEqual(mask.shape[0], dset.data.shape[0])
-            self.assertEqual(mask.shape[1], dset.data.shape[1])
-            self.assertEqual(mask.dtype, bool)
-
-            if self.debug:
-                plt.style.use('dark_background')
-                fig, ax = plt.subplots(1, 1, figsize=(7,7))
-                im = ax.imshow(mask)
-                fig.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
-                plt.tight_layout()
-                plt.show()
-
-    def test_integrate(self):
-        for reader, data_name in zip(self.readers, self.data_names):
-            dset = darling.DataSet(reader)
-            dset.load_scan(data_name, scan_id="1.1", roi=None)
-            int_frames = dset.integrate()
-            self.assertEqual(int_frames.shape[0], dset.data.shape[0])
-            self.assertEqual(int_frames.shape[1], dset.data.shape[1])
-            self.assertEqual(int_frames.dtype, np.float32)
-
-            if self.debug:
-                plt.style.use('dark_background')
-                fig, ax = plt.subplots(1, 1, figsize=(7,7))
-                im = ax.imshow(int_frames)
-                fig.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
-                plt.tight_layout()
-                plt.show()
-    
-    def test_compile_layers(self):
-
-        # test that the mosa reader will stack layers
-        path_to_data, _, _ = darling.assets.mosaicity_scan()
-        reader = darling.reader.MosaScan(
-            path_to_data,
-            ["instrument/chi/value", "instrument/diffrz/data"],
-            motor_precision=[3, 3],
-        )
-
-        data_name = "instrument/pco_ff/image"
-        dset_mosa = darling.DataSet(reader)
-
-        mean_3d, cov_3d = dset_mosa.compile_layers(
-            data_name, scan_ids=["1.1", "2.1"], verbose=False
-        )
-
-        self.assertEqual(mean_3d.shape[0], 2)
-        self.assertEqual(len(mean_3d.shape), 4)
-        self.assertEqual(cov_3d.shape[0], 2)
-        self.assertEqual(len(cov_3d.shape), 5)
-
-        # test that the energy reader can stack layers.
-        path_to_data, _, _ = darling.assets.energy_scan()
-        data_name = "instrument/pco_ff/data"
-        reader = darling.reader.EnergyScan(
-        path_to_data,
-        ["instrument/positioners/ccmth", "instrument/chi/value"],
-        motor_precision=[4, 4],
-        )
-        dset_energy = darling.DataSet(reader)
-        mean_3d, cov_3d = dset_energy.compile_layers(
-            data_name, scan_ids=["1.1", "2.1"], verbose=False
-        )
-
-        self.assertEqual(mean_3d.shape[0], 2)
-        self.assertEqual(len(mean_3d.shape), 4)
-        self.assertEqual(cov_3d.shape[0], 2)
-        self.assertEqual(len(cov_3d.shape), 5)
-
-    def test_as_paraview(self):
-        path_to_data, _, _ = darling.assets.mosaicity_scan()
-        reader = darling.reader.MosaScan(
-            path_to_data,
-            ["instrument/chi/value", "instrument/diffrz/data"],
-            motor_precision=[3, 3],
-        )
-
-        data_name = "instrument/pco_ff/image"
-        dset_mosa = darling.DataSet(reader)
-
-        mean_3d, cov_3d = dset_mosa.compile_layers(
-            data_name, scan_ids=["1.1", "2.1"], verbose=False
-        )
-
-        filename = os.path.join(darling.assets.path(), 'saves', 'mosa_stack')
-        dset_mosa.to_paraview(filename)
-
-        path_to_data, _, _ = darling.assets.energy_scan()
-        data_name = "instrument/pco_ff/data"
-        reader = darling.reader.EnergyScan(
-        path_to_data,
-        ["instrument/positioners/ccmth", "instrument/chi/value"],
-        motor_precision=[4, 4],
-        )
-        dset_energy = darling.DataSet(reader)
-        mean_3d, cov_3d = dset_energy.compile_layers(
-            data_name, scan_ids=["1.1", "2.1"], verbose=False
-        )
-
-        filename = os.path.join(darling.assets.path(), 'saves', 'energy_stack')
-        dset_energy.to_paraview(filename)
-
-
-    def check_data(self, dset):
-        self.assertTrue(dset.data.dtype == np.uint16)
-        self.assertTrue(len(dset.data.shape) == 4)
-        self.assertTrue(dset.data.shape[2] == len(dset.motors[0]))
-        self.assertTrue(dset.data.shape[3] == len(dset.motors[1]))
-        self.assertTrue(dset.motors[0].dtype == np.float32)
-        self.assertTrue(dset.motors[1].dtype == np.float32)
-        self.assertTrue(len(dset.motors[0].shape) == 1)
-        self.assertTrue(len(dset.motors[1].shape) == 1)
 
 if __name__ == "__main__":
     unittest.main()